0109ae38ae06a23f8e4ffb2d39acc8500e8f46b5
MicheleSr <mikefender@cryptolab.net>
2014-08-26 18:28:09 +0200

    implement user add

--- project/Controllers/UsersController.cs
+++ project/Controllers/UsersController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Collections;
 using System.Linq;
+using System.IO;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Mvc.Ajax;
+using Newtonsoft.Json.Linq;
 using project.Models;
 using project.Utils;
 
@@ -32,14 +34,31 @@ namespace project.Controllers {
 			if (k.isAdmin() || k.checkUser(id))
 			    return Json(Model.getHashtableById<Models.User>(id).filterPassword(), JsonRequestBehavior.AllowGet);
 			else
-				return Json(Utils.Costants.unauthorized, JsonRequestBehavior.AllowGet);
+				return Json(Utils.Costants.UNAUTHORIZED, JsonRequestBehavior.AllowGet);
 		}
 
 		// /#/user/1/create
 		// POST /api/user/add/
 		[AcceptVerbs(HttpVerbs.Post)]
-		public String Index(int id, String data) {
-			return "add user " +  "id " + id;
+		public JsonResult Index() {
+			String data = new StreamReader(Request.InputStream).ReadLine();
+			ConvertibleHashtable h = ConvertibleHashtable.fromString(data);
+			ConvertibleHashtable ud = ((JObject)h ["user_data"]).ToObject<ConvertibleHashtable>();
+			ApiKey k = ApiKey.getApiKey();
+			if (k.isAdmin ()) {
+				if (h ["type"].ToString () == "supplier") {
+					ConvertibleHashtable sd = ((JObject)h ["supplier_data"]).ToObject<ConvertibleHashtable> ();
+					new Supplier (ud ["email"].ToString (), ud ["password"].ToString (), ud ["first_name"].ToString (), ud ["last_name"].ToString (), 
+						sd ["vat"].ToString (), sd ["supplier_name"].ToString (), sd ["city"].ToString ()).insert ();
+				} 
+				else if (h ["type"].ToString () == "admin") 
+					new Admin (ud ["email"].ToString (), ud["password"].ToString (), ud["first_name"].ToString(), ud["last_name"].ToString()).insert();
+				else
+      				return Json(Utils.Costants.WRONG_USER_TYPE, JsonRequestBehavior.AllowGet);
+				return Json(Utils.Costants.OK, JsonRequestBehavior.AllowGet);
+			}
+			else 
+				return Json(Utils.Costants.UNAUTHORIZED, JsonRequestBehavior.AllowGet);
 		}
 
 		/*public String Test(String data) {
--- project/Utils/Costants.cs
+++ project/Utils/Costants.cs
@@ -4,9 +4,23 @@ namespace project.Utils
 {
 	public class Costants
 	{
-		public static ConvertibleHashtable unauthorized { get { 
-				ConvertibleHashtable h = new ConvertibleHashtable ();
+		public static ConvertibleHashtable UNAUTHORIZED { get { 
+				ConvertibleHashtable h = new ConvertibleHashtable();
 				h.Add ("error", "user_unauthorized");
+				return h;
+			} 
+		}
+
+		public static ConvertibleHashtable OK { get { 
+				ConvertibleHashtable h = new ConvertibleHashtable ();
+				h.Add ("server", "success");
+				return h;
+			} 
+		}
+
+		public static ConvertibleHashtable WRONG_USER_TYPE { get { 
+				ConvertibleHashtable h = new ConvertibleHashtable ();
+				h.Add ("error", "wrong user type");
 				return h;
 			} 
 		}
