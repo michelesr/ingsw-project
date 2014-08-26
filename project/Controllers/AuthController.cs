ccf8837997bbfec2d454f210ce2842a1e0911c45
MicheleSr <mikefender@cryptolab.net>
2014-08-26 20:35:51 +0200

    minor change

--- project/Controllers/AuthController.cs
+++ project/Controllers/AuthController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Mvc.Ajax;
@@ -13,7 +14,7 @@ namespace project.Controllers {
 
 		[AcceptVerbs(HttpVerbs.Post)]
 		public JsonResult Index(int id, String data) {
-			ConvertibleHashtable d = ConvertibleHashtable.fromString(data); 
+            ConvertibleHashtable d = JObject.Parse(new StreamReader(Request.InputStream).ReadLine()).ToObject<ConvertibleHashtable>();
 			ConvertibleHashtable h = new ConvertibleHashtable();
 			h.Add("auth", Models.User.checkPassword(d["email"].ToString(), d["password"].ToString()));
 			if((bool) h["auth"]) {
