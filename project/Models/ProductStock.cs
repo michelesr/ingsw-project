40e475c13a4a1874200bd34e9eb8bc10bebff58e
MicheleSr <mikefender@cryptolab.net>
2014-09-15 17:19:18 +0200

    add input control on stocks
    api will throw "invalid format exception" on invalid input

--- project/Models/ProductStock.cs
+++ project/Models/ProductStock.cs
@@ -14,6 +14,26 @@ namespace project.Models
         // disponibilità
         public int availability { get; set; }
 
+        // costruttore che controlla i campi
+        public ProductStock(int product_id, double price, int min, int max, int availability) : base() {
+            Console.WriteLine("p_id " + product_id +   " price " + price + " min" + min + " max" + max +  "avai " + availability);
+            if (min > 0 && min <= max && price > 0 && availability >= 0) {
+                this.min = min;
+                this.max = max;
+                this.price = price;
+                this.availability = availability;
+                this.product_id = product_id;
+            }
+            else 
+                throw new FormatException();
+        }
+
+        public override void update() {
+            if (min < 0 || min > max || price <= 0 || availability < 0)
+                throw new FormatException();
+            base.update();
+        }
+
         // controlla che l'user_id sia relativo al produttore del prodotto in stock
         public bool checkUserId(int uid) {
             return Model.getById<Product>(product_id).checkUserId(uid);
