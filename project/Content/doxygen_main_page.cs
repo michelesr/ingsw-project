ee4b901bde2d2d907bb5caa6cfe43dfdb66e87b5
MicheleSr <mikefender@cryptolab.net>
2014-09-18 20:40:15 +0200

    add documentation main page

---
+++ project/Content/doxygen_main_page.cs
@@ -0,0 +1,9 @@
+/*! \mainpage Progetto Ingegneria del Software
+ *
+ * \section intro_sec Introduzione
+ *
+ * Questa è la pagina principale della documentazione, è possibile navigare nella documentazione attraverso il menù in cima alla pagina.
+
+ Autori: Michele Sorcinelli, Antonio Esposito.
+ *
+ */
